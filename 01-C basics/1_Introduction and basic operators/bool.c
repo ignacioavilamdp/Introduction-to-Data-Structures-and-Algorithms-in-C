48b8e1c4229c4343cc6ae3c62f786bde3eb09cd5
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-07-18 10:09:59 -0300

    First files

---
+++ 01-C basics/1_Introduction and basic operators/bool.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * C does not include a primitive "boolean" data type. "Int" type is used for such purposes.
+ * In C a logical expression is considered to be false if it is evaluated to 0 and true in any other case.
+ * So, "true" equals "not zero" (K&R Chap 2.7)
+
+*/
+
+int main(){
+
+    if(-2)
+        printf("This is to show that ""-2"" is considered true \n");
+
+    if(-1)
+        printf("This is to show that ""-1"" is considered true \n");
+
+    if(0)
+        printf("This is to show that ""0"" is considered false \n");
+
+    if(1)
+        printf("This is to show that ""1"" is considered true \n");
+
+    if(2)
+        printf("This is to show that ""2"" is considered true \n");
+
+    return 0;
+}
